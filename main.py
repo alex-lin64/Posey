354f29ddc03554bce9fab4eb69af35a7ba9bb6a1
alex-lin64 <alex_lin@brown.edu>
2023-08-31 13:47:16 -0400

    adding daemons

--- main.py
+++ main.py
@@ -4,30 +4,59 @@ import numpy as np
 import tensorflow as tf
 import pyfirmata
 
+from threading import Thread
+import time
+
 from utils.processing import preprocess
 
+
+# global vars
+SQUAT_COUNT = 0
+POSITION = 1  # 1 is up position, 0 is down
+
+
+def update_count():
+    """
+    Monitors current squat position and updates the squat count accordingly.  Built
+    to run as a daemon thread
+    """
+    global SQUAT_COUNT
+    global POSITION
+    prev_pos = POSITION
+
+    while True:
+        if prev_pos and not POSITION:
+            SQUAT_COUNT += 1
+        prev_pos = POSITION
+        time.sleep(0.1)
 
 def main():
     """
     Main loop for Posey
     """
+    global POSITION
+
     # init mp pose objects
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
+
     # load TFLite model, allocate tensors
     interpreter = tf.lite.Interpreter(model_path='model\\tf_lite_model\\squat_classifier.tflite')
     interpreter.allocate_tensors()
+
     # Get input and output tensors.
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
+
     # start video capture
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    # init squat count vars
-    squat_count = 0
-    prev_position = "up"
-    position = "up"
+
+    print('Starting squat count task...')
+    count_daemon = Thread(target=update_count, daemon=True, name='squat_count')
+    count_daemon.start()
+
     # only init arduino if it is being used
     board = None
     try:
@@ -38,7 +67,7 @@ def main():
     # start live stream 
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
-            ret, frame = cap.read()
+            _, frame = cap.read()
             
             # changing display colors
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -60,25 +89,28 @@ def main():
             try:
                 # set input data to model
                 interpreter.set_tensor(input_details[0]['index'], input_data)
+
                 # squat classifier - invoke inference
                 interpreter.invoke()
+
                 # squat classifier - invoke inference
-                # index 0 is down position, index 1 is up position
                 output_data = interpreter.get_tensor(output_details[0]['index'])
                 down, up = output_data[0][0], output_data[0][1]
+
                 # interpret results
-                prev_position = position
-                position = "up" if up >= down else "down"
+
+                # prev_position = position
+                POSITION = 1 if up >= down else 0
                 probability = str(round(up, 2)) if up >= down else str(round(down, 2))
 
-                # count squats
-                if position == "down" and prev_position == "up":
-                    squat_count += 1
+                # # count squats
+                # if position == "down" and prev_position == "up":
+                #     squat_count += 1
 
                 # display squat classification
                 cv2.putText(
                     img=frame,
-                    text=f"Count: {str(squat_count)}",
+                    text=f"Count: {str(SQUAT_COUNT)}",
                     org=(10, 30),
                     fontFace=cv2.FONT_HERSHEY_DUPLEX,
                     fontScale=1,
@@ -90,7 +122,7 @@ def main():
                 # display squat classification
                 cv2.putText(
                     img=frame,
-                    text=position,
+                    text=("up" if POSITION else "down"),
                     org=(10, frame.shape[0] - 40),
                     fontFace=cv2.FONT_HERSHEY_DUPLEX,
                     fontScale=1,
